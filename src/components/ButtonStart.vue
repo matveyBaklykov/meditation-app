f7969ed79989f2bbbee00648478794b52c9b24df
Matvey Baklykov <matvey.bakl@gmail.com>
2025-07-13 13:13:28 +0300

    added meditation cards, meditation store

---
+++ src/components/ButtonStart.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import IconPlay from '@/icons/IconPlay.vue'
+</script>
+
+<template>
+  <button class="button-start">
+    <div>Начать</div>
+    <IconPlay />
+  </button>
+</template>
+
+<style scoped>
+.button-start {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 11px;
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+  background-color: var(--color-dark-green);
+  color: var(--color-text);
+  padding-block: 10px;
+  transition: all 0.2s;
+}
+
+.button-start:hover {
+  background-color: var(--color-light-green);
+}
+</style>
